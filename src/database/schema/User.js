9e01c0c0c587ad2bb3b22e62f5754140ed98350d
max.cheung <cheungwaiman1919@gmail.com>
2022-11-23 18:53:44 +0800

    commit

---
+++ src/database/schema/User.js
@@ -0,0 +1,10 @@
+export const User = {
+    name: "User",
+    properties: {
+      _id: "int",
+      email: "string",
+      timeStamp: "date",
+      status: "string?",
+    },
+    primaryKey: "_id",
+  };
