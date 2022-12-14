23979038c2e226b77bedeeff232a65fefc9516c3
max <cheungwaiman1919@gmail.com>
2022-12-15 00:15:38 +0800

    fixed drawer bug, style issue

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -9,84 +9,187 @@ import {
 } from 'react-native';
 import {
   DrawerContentScrollView,
+  DrawerItem,
   DrawerItemList,
 } from '@react-navigation/drawer';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import { AuthContext } from '../context/AuthContext';
+import {AuthContext} from '../context/AuthContext';
+import {Drawer, Switch, TouchableRipple} from 'react-native-paper';
 
 const CustomDrawer = props => {
-
-    const {userInfo, splashLoading} = useContext(AuthContext);
+  const {userInfo, splashLoading, logout} = useContext(AuthContext);
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
   return (
     <View style={{flex: 1}}>
-      <DrawerContentScrollView
-        {...props}
-        contentContainerStyle={{backgroundColor: '#8200d6'}}>
-        <ImageBackground
-          source={require('../images/menu-bg.jpeg')}
-          style={{padding: 20}}>
-          <Image
-            source={require('../images/user-profile.jpg')}
-            style={{height: 80, width: 80, borderRadius: 40, marginBottom: 10}}
-          />
-          <Text
-            style={{
-              color: '#fff',
-              fontSize: 18,
-              fontFamily: 'Roboto-Medium',
-              marginBottom: 5,
-            }}>
-            John Doe
-          </Text>
-          <View style={{flexDirection: 'row'}}>
-            <Text
-              style={{
-                color: '#fff',
-                fontFamily: 'Roboto-Regular',
-                marginRight: 5,
-              }}>
-              280 Coins
-            </Text>
-            <FontAwesome5 name="coins" size={14} color="#fff" />
+      {splashLoading ? (
+        <Drawer.Screen name="Splash Screen" component={SplashScreen} />
+      ) : (
+        <>
+          <DrawerContentScrollView
+            {...props}
+            contentContainerStyle={{backgroundColor: '#8200d6'}}>
+            <ImageBackground
+              source={require('../images/menu-bg.jpeg')}
+              style={{padding: 20}}>
+              <Image
+                source={require('../images/user-profile.jpg')}
+                style={{
+                  height: 80,
+                  width: 80,
+                  borderRadius: 40,
+                  marginBottom: 10,
+                }}
+              />
+              {userInfo.token ? (
+                <>
+                  <Text
+                    style={{
+                      color: '#fff',
+                      fontSize: 18,
+                      fontFamily: 'Roboto-Medium',
+                      marginBottom: 5,
+                    }}>
+                    {userInfo.fullname}
+                  </Text>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text
+                      style={{
+                        color: '#fff',
+                        fontFamily: 'Roboto-Regular',
+                        marginRight: 5,
+                      }}>
+                      {userInfo.usermail}
+                    </Text>
+
+                    <FontAwesome5 name="mail-bulk" size={14} color="#fff" />
+                  </View>
+                </>
+              ) : (
+                ''
+              )}
+            </ImageBackground>
+            <View style={{flex: 1, backgroundColor: '#fff', paddingTop: 10}}>
+              {/* <DrawerItemList {...props} /> */}
+              {userInfo.token ? (
+                <>
+                  <DrawerItem
+                    activeBackgroundColor={'#aa18ea'}
+                    activeTintColor={'#fff'}
+                    inactiveTintColor={'#333'}
+                    labelStyle={{fontFamily: 'Roboto-Medium', fontSize: 15}}
+                    icon={() => <Ionicons name="home-outline" size={22} />}
+                    label={'CMMS'}
+                    onPress={() => {
+                      props.navigation.navigate('CMMS');
+                    }}
+                  />
+
+                  <DrawerItem
+                    activeBackgroundColor={'#aa18ea'}
+                    activeTintColor={'#fff'}
+                    inactiveTintColor={'#333'}
+                    labelStyle={{fontFamily: 'Roboto-Medium', fontSize: 15}}
+                    icon={() => (
+                      <Ionicons name="speedometer-outline" size={22} />
+                    )}
+                    label={'Stress Tests'}
+                    onPress={() => {
+                      props.navigation.navigate('Stress Test');
+                    }}
+                  />
+                </>
+              ) : (
+                <>
+                  <DrawerItem
+                    activeBackgroundColor={'#aa18ea'}
+                    activeTintColor={'#fff'}
+                    inactiveTintColor={'#333'}
+                    labelStyle={{fontFamily: 'Roboto-Medium', fontSize: 15}}
+                    icon={() => <Ionicons name="log-in-outline" size={22} />}
+                    label={'Login'}
+                    onPress={() => {
+                      props.navigation.navigate('Login');
+                    }}
+                  />
+
+                  <DrawerItem
+                    activeBackgroundColor={'#aa18ea'}
+                    activeTintColor={'#fff'}
+                    inactiveTintColor={'#333'}
+                    labelStyle={{fontFamily: 'Roboto-Medium', fontSize: 15}}
+                    icon={() => (
+                      <Ionicons name="person-add-outline" size={22} />
+                    )}
+                    label={'Register'}
+                    onPress={() => {
+                      props.navigation.navigate('Register');
+                    }}
+                  />
+                </>
+              )}
+              <TouchableRipple>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingVertical: 12,
+                    paddingHorizontal: 16,
+                  }}>
+                  <Text>Dark Mode</Text>
+                  <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+                </View>
+              </TouchableRipple>
+            </View>
+          </DrawerContentScrollView>
+          <View
+            style={{padding: 20, borderTopWidth: 1, borderTopColor: '#ccc'}}>
+            {userInfo.token ? (
+              <>
+                <TouchableOpacity
+                  onPress={() => {
+                    props.navigation.navigate('Setting');
+                  }}
+                  style={{paddingVertical: 15}}>
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Ionicons name="ios-settings-outline" size={22} />
+                    <Text
+                      style={{
+                        fontSize: 15,
+                        fontFamily: 'Roboto-Medium',
+                        marginLeft: 5,
+                      }}>
+                      Setting
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    logout();
+                  }}
+                  style={{paddingVertical: 15}}>
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Ionicons name="exit-outline" size={22} />
+                    <Text
+                      style={{
+                        fontSize: 15,
+                        fontFamily: 'Roboto-Medium',
+                        marginLeft: 5,
+                      }}>
+                      Sign Out
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </>
+            ) : (
+              ''
+            )}
           </View>
-        </ImageBackground>
-        <View style={{flex: 1, backgroundColor: '#fff', paddingTop: 10}}>
-          <DrawerItemList {...props} />
-        </View>
-      </DrawerContentScrollView>
-      <View style={{padding: 20, borderTopWidth: 1, borderTopColor: '#ccc'}}>
-        <TouchableOpacity onPress={() => {navigation.openDrawer("Setting")}} style={{paddingVertical: 15}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Ionicons name="ios-settings-outline" size={22} />
-            <Text
-              style={{
-                fontSize: 15,
-                fontFamily: 'Roboto-Medium',
-                marginLeft: 5,
-              }}>
-              Setting
-            </Text>
-          </View>
-        </TouchableOpacity>
-        {userInfo.token ?
-        <TouchableOpacity onPress={() => {}} style={{paddingVertical: 15}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Ionicons name="exit-outline" size={22} />
-            <Text
-              style={{
-                fontSize: 15,
-                fontFamily: 'Roboto-Medium',
-                marginLeft: 5,
-              }}>
-              Sign Out
-            </Text>
-          </View>
-        </TouchableOpacity> : ""
-        }
-      </View>
+        </>
+      )}
     </View>
   );
 };
--- src/components/DrawerNavigator.js
+++ src/components/DrawerNavigator.js
@@ -6,7 +6,7 @@ import StressTestScreen from '../screens/StressTestScreen';
 import SplashScreen from '../screens/SplashScreen';
 import {AuthContext} from '../context/AuthContext';
 import RegisterScreen from '../screens/RegisterScreen';
-import { useContext } from 'react';
+import {useContext} from 'react';
 import CustomDrawer from './CustomDrawer';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -17,52 +17,72 @@ const DrawerNavigator = () => {
 
   return (
     <Drawer.Navigator
-    drawerContent={props => <CustomDrawer {...props} />}
-    screenOptions={{
-      headerShown: false,
-      drawerActiveBackgroundColor: '#aa18ea',
-      drawerActiveTintColor: '#fff',
-      drawerInactiveTintColor: '#333',
-      drawerLabelStyle: {
-        //marginLeft: -25,
-        fontFamily: 'Roboto-Medium',
-        fontSize: 15,
-      },
-    }}>
+      drawerContent={props => <CustomDrawer {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerActiveBackgroundColor: '#aa18ea',
+        drawerActiveTintColor: '#fff',
+        drawerInactiveTintColor: '#333',
+        drawerLabelStyle: {
+          //marginLeft: -25,
+          fontFamily: 'Roboto-Medium',
+          fontSize: 15,
+        },
+      }}>
       {splashLoading ? (
         <Drawer.Screen name="Splash Screen" component={SplashScreen} />
       ) : userInfo.token ? (
         <>
-          <Drawer.Screen name="CMMS" component={BottomTabNavigator} options={{
-          drawerIcon: ({color}) => (
-            <Ionicons name="home-outline" size={22} color={color} />
-          ),
-        }}/>
-          <Drawer.Screen name="Stress Test" component={StressTestScreen} options={{
-          drawerIcon: ({color}) => (
-            <Ionicons name="speedometer-outline" size={22} color={color} />
-          ),
-        }}/>
-          <Drawer.Screen name="Setting" component={SettingScreen} options={{
-          drawerIcon: ({color}) => (
-            <Ionicons name="settings-outline" size={22} color={color} />
-          ),
-        }}/>
+          <Drawer.Screen
+            name="CMMS"
+            component={BottomTabNavigator}
+            options={{
+              drawerIcon: ({color}) => (
+                <Ionicons name="home-outline" size={22} color={color} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Stress Test"
+            component={StressTestScreen}
+            options={{
+              drawerIcon: ({color}) => (
+                <Ionicons name="speedometer-outline" size={22} color={color} />
+              ),
+            }}
+          />
         </>
       ) : (
         <>
-          <Drawer.Screen name="Login" component={LoginScreen} options={{
-          drawerIcon: ({color}) => (
-            <Ionicons name="log-in-outline" size={22} color={color} />
-          ),
-        }}/>
-          <Drawer.Screen name="Register" component={RegisterScreen} options={{
-          drawerIcon: ({color}) => (
-            <Ionicons name="person-add-outline" size={22} color={color} />
-          ),
-        }}/>
+          <Drawer.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{
+              drawerIcon: ({color}) => (
+                <Ionicons name="log-in-outline" size={22} color={color} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{
+              drawerIcon: ({color}) => (
+                <Ionicons name="person-add-outline" size={22} color={color} />
+              ),
+            }}
+          />
         </>
       )}
+      <Drawer.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{
+          drawerIcon: ({color}) => (
+            <Ionicons name="settings-outline" size={22} color={color} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 };
